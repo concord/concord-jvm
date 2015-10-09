1bd5597ccc6124c8db6f595efab37d92d943ca85
Rob Blafford <rob@concord.io>
2015-10-09 14:53:35 -0400

    Removing getting_started demo

--- getting_started/runner.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash --login
-dir=$(pwd)
-jar_file=$(find $dir -iname "getting_started-assembly-*.jar");
-if [[ ! -f $jar_file ]]; then
-    echo "Cannot find getting_started assembly file";
-    exit 1
-fi
-echo "Found java executable at: $jar_file"
-exec java -cp $jar_file $@
