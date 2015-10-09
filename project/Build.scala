35f6ef4b3ab116dccb8ef659db7b6ccfeb18ef10
Rob Blafford <rob@concord.io>
2015-10-09 17:51:55 -0400

    Fixing build system
    - Build settings were never included

--- project/Build.scala
+++ project/Build.scala
@@ -5,32 +5,25 @@ import sbtassembly.AssemblyKeys._
 import sbtassembly._
 
 object ConcordJvmBuild extends Build {
-  lazy val scalaSettings = Seq(
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     scalaVersion := "2.11.6",
-    version := "0.1.0",
-    scalacOptions := Seq("-Xlint", "-deprecation", "-Xfatal-warnings")
+    organization := "io.concord",
+    libraryDependencies ++= Seq(
+      "com.facebook.swift" % "swift-service" % "0.14.1",
+      "com.facebook.swift" % "swift-codec" % "0.14.1"
+    ),
+    autoScalaLibrary := false,
+    crossPaths := false
   )
 
-  assemblyMergeStrategy in assembly := {
-    case x if x.endsWith("project.clj") => MergeStrategy.discard
-    case x if x.toLowerCase.startsWith("meta-inf") => MergeStrategy.discard
-    case _ => MergeStrategy.first
-  }
-
   lazy val rawapi = project
-    .settings(crossPaths := false, autoScalaLibrary := false) // java only
+    .settings(buildSettings: _*)
 
   lazy val concord_java = project
-    .settings(crossPaths := false, autoScalaLibrary := false) // java only
     .dependsOn(rawapi)
-
-
-  lazy val getting_started  = project
-    .settings(scalaSettings: _*)
-    .dependsOn(rawapi)
-    .dependsOn(concord_java)
-
+    .settings(buildSettings: _*)
 
   lazy val root = (project in file("."))
-    .aggregate(rawapi, concord_java, getting_started)
+    .aggregate(rawapi, concord_java)
+    .settings(buildSettings: _*)
 }
--- rawapi/build.sbt
+++ rawapi/build.sbt
@@ -1,6 +1,5 @@
-libraryDependencies ++= Seq(
-  "com.facebook.swift" % "swift-service" % "0.14.1",
-  "com.facebook.swift" % "swift-codec" % "0.14.1"
-)
+name := "rawapi"
+
+version := "0.1.0"
 
 javaSource in Compile := baseDirectory.value / "gen-swift"
